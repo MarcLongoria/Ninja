e2d3e7266caf23439d20ff7bdac33afacacdf959
Marc Longoria <marclongoria@outlook.com>
2017-08-04 16:37:04 -0500

    Comitting domain.

---
+++ SamuriAppCore/SamuriAppCore.Domain/Samurai.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuriAppCore.Domain
+{
+    public class Samurai
+    {
+        public Samurai()
+        {
+            Qoutes = new List<Quote>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Qoutes { get; set; }
+        public int Battlefield { get; set; }
+    }
+}
