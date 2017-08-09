cce8c55fb72eaba43ac7999e2ca9b036e3a2f542
Marc Longoria <marclongoria@outlook.com>
2017-08-09 16:03:26 -0500

    Commiting executable for EF CLI tools and first migration to localdb

--- SamuriAppCore/SamuraiAppCore.Data/SamuraiContext.cs
+++ SamuriAppCore/SamuraiAppCore.Data/SamuraiContext.cs
@@ -10,8 +10,14 @@ namespace SamuraiAppCore.Data
 {
     public class SamuraiContext : DbContext
     {
-        //public DbSet<Battle> Battles { get; set; } 
-        //public DbSet
-        
+        public DbSet<Battle> Battles { get; set; } 
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= (localdb)\\mssqllocaldb; Database = SamuraiDataCore; Trusted_Connection = True;");
+        }
+
     }
 }
