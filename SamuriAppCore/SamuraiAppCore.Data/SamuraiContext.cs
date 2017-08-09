73e71add15120a72768030ab8b8f80527ba0e04e
Marc Longoria <marclongoria@outlook.com>
2017-08-09 10:45:30 -0500

    Fixed stupid domain

--- SamuriAppCore/SamuraiAppCore.Data/SamuraiContext.cs
+++ SamuriAppCore/SamuraiAppCore.Data/SamuraiContext.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SamuraiAppCore.Domain;
 
 namespace SamuraiAppCore.Data
 {
     public class SamuraiContext : DbContext
     {
-
+        //public DbSet<Battle> Battles { get; set; } 
+        //public DbSet
         
     }
 }
